bb1adbc8b73764ea988fe9ec047295f5444772a4
Damilola <omifaredammy@gmail.com>
2020-01-05 22:43:16 +0000

    Customize the Row Preview 😀 - update the landmark parameter to be the second element in the landmarkData array. - Grouped landmark row 👍🏾

--- BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkRow.swift
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkRow.swift
@@ -13,7 +13,7 @@ struct LandmarkRow: View {
     var landmark: Landmark
 
     var body: some View {
-        HStack {
+        HStack() {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
@@ -24,6 +24,10 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarkData[0])
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
